22b25d00dbe055b367bddd7725ff40cfeb99db38
t-ngtn <obutaiei@gmail.com>
2022-08-24 21:53:28 +0900

    add: network

--- scripts/lif/network.jl
+++ scripts/lif/network.jl
@@ -10,9 +10,13 @@ const T = 1000.0
 const NT = 1000
 const I_EXT = 12.0
 const N = 2
+const TAU_SYN = 5.0
+const R_SYN = 1.0
+const W = 2.0
 
 function main()
     v = [V_REST, V_REST - 15.0]
+    i_syn = [0.0, 0.0]
     s = [false, false]
 
     tl = [[], []]
@@ -27,7 +31,11 @@ function main()
         end
 
         for i in 1:N
-            v[i] += DT * (-(v[i] - V_REST) + R_M * I_EXT) / TAU
+            s_op = i == 1 ? 2 : 1
+            i_syn[i] = exp.(-DT / TAU_SYN) * i_syn[i] + W * s_op
+        end
+        for i in 1:N
+            v[i] += DT * (-(v[i] - V_REST) + R_SYN * i_syn[i] + R_M * I_EXT) / TAU
             s[i] = (v[i] > THETA)
         end
 
@@ -45,9 +53,9 @@ function main()
         end
     end
 
-    plot(tl[1], vl[1], fmt=:png, title="lif model(2 neuron)", xlabel="time[m/s]", ylabel="membrane potential[mV]", label="v1", ylims=(-70, 0))
+    plot(tl[1], vl[1], fmt=:png, title="lif model(2 neuron network)", xlabel="time[m/s]", ylabel="membrane potential[mV]", label="v1", ylims=(-70, 0))
     plot!(tl[2], vl[2], label="v2")
-    png("figures/lif/lif2.png")
+    png("figures/lif/network.png")
 
 end
 
